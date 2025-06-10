0f6535450a239bec2425dbf6483c2c306a615fb8
Collinsnwoye <160544205+Collinsnwoye@users.noreply.github.com>
2025-06-10 13:08:43 +0100

    Add files via upload

---
+++ SQLQuery1.3.sql
@@ -0,0 +1,7 @@
+SELECT custid, orderid, orderdate, freight
+FROM (
+    SELECT TOP 3
+        custid, orderid, orderdate, freight
+    FROM Sales.Orders
+    ORDER BY custid, orderdate DESC
+) AS RecentOrders;
